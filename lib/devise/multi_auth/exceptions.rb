5e2cf8ff4be79a1e43d89905a70e00de91890869
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-06 10:07:37 -0600

    Updating Devise::MultiAuth exceptions messages
    I want to expose helpful messages.

--- lib/devise/multi_auth/exceptions.rb
+++ lib/devise/multi_auth/exceptions.rb
@@ -3,13 +3,20 @@ module Devise::MultiAuth
     def initialize(options = {})
       provider = options.fetch(:provider)
       uid = options.fetch(:uid)
-      super("Error with provider: #{provider.inspect}, uid: #{uid.inspect}")
+      super("#{message_prefix} provider: #{provider.inspect}, uid: #{uid.inspect}.")
+    end
+    def message_prefix
+      "Unspecified access token error for"
     end
   end
 
   class AccessTokenNotFound < AccessTokenError
+    def message_prefix
+      "Unable to find access token for"
+    end
   end
 
   class AccessTokenUnverified < AccessTokenError
+    "Possible access token found but unverified for"
   end
 end
